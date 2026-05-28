391dc875bcd132687c37efacb81580015ed79f2d
Thanh Hoang <hoang_thanh15@yahoo.com>
2026-05-27 17:26:37 -0700

    add Leads by sourcechart

--- admin-dashboard-react/src/components/pieChart/Piechart.tsx
+++ admin-dashboard-react/src/components/pieChart/Piechart.tsx
@@ -1,5 +1,5 @@
 import "./pieChart.scss"
-import { Pie, PieChart } from 'recharts';
+import { Pie, PieChart,Tooltip } from 'recharts';
 
 
 
@@ -15,20 +15,50 @@ export const Piechart = () => {
   return (
     <div className="pieChart">
         <div className="label">Leads By Source</div>
-         <PieChart style={{ width: '100%', maxWidth: '500px', maxHeight: '80vh', aspectRatio: 1 }} responsive>
+        <PieChart
+            style={{
+            width: '100%',
+            maxWidth: '500px',
+            maxHeight: '80vh',
+            aspectRatio: 1,
+            }}
+            responsive
+        >
+            <Tooltip
+            formatter={(value, name) => [`${value}%`, name]}
+            contentStyle={{
+                background: 'transparent',
+                border: 'none',
+                boxShadow: 'none',
+            }}
+            itemStyle={{
+                color: 'white',
+            }}
+            />
             <Pie
-
-                data={pieChartData}
-                dataKey="value"
-                nameKey="name"
-                innerRadius="70%"
-                outerRadius="90%"
-                cornerRadius={50}
-                paddingAngle={5}
-
-             
+            data={pieChartData}
+            dataKey="value"
+            nameKey="name"
+            innerRadius="70%"
+            outerRadius="90%"
+            cornerRadius={50}
+            paddingAngle={5}
             />
         </PieChart>
+        <div className="chartInfo">
+            <div className="mobile">
+                <span>O</span>
+                <div className="mobileLabel"> Mobile</div>
+            </div>
+            <div className="desktop">
+                <span>O</span>
+                <div className="desktopLabel"> Desktop</div>          
+            </div>
+            <div className="laptop">
+                <span>O</span>
+                <div className="laptopLabel">Laptop</div>
+            </div>
+        </div>
 
     </div>
   )

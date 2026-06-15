bdc8b8f59639a02763f3405cdc0d0c56b7738ea1
Thanh Hoang <hoang_thanh15@yahoo.com>
2026-06-14 19:12:15 -0700

    add notification

--- admin-dashboard-react/src/components/addUser/Adduser.tsx
+++ admin-dashboard-react/src/components/addUser/Adduser.tsx
@@ -8,46 +8,50 @@ type Props = {
   setOpen: Dispatch<SetStateAction<boolean>>;
 };
 
-
 export const Adduser = (props: Props) => {
-    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-    }
-    return (
-        <div className="addUser">
-            <div className="modal">
-                <div className="close" onClick={() => props.setOpen(false)}>X</div>
-                <div className="title">Add new user</div>
-                <form action="" className="newUserInfo" onSubmit={handleSubmit}>
-                    <div className="allField">
-                        {props.column.filter(filter => {
-                            return (
-                                filter.headerName != "ID" && filter.headerName != "Action" && filter.headerName != "Age"
-                            );
-                        }
-                        ).map(column => {
-                            return (
-                                <div className="userBox" key={column.field}>
-                                    <div className="label"> {column.headerName}</div>
-                                    <input name={column.field} type={column.type} placeholder={column.field} className="userInput" />
-                        
-                                </div>
-                        
-                            );
-                        }
-                        )}
-                        <label className="uploadImg">
-                            <div>Upload Avatar Image:</div>
-                            <img src="img.svg" alt="" />
-                            <input type="file" accept="image*"   style={{display: "none"}}  />
-                        </label>
-                    </div>
-
-
-
-                    <button> Send </button>
-                </form>
-            </div>
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+  return (
+    <div className="addUser" onClick={() => props.setOpen(false)}>  
+      <div className="modal" onClick={(e) => e.stopPropagation()}>  
+        <div className="close" onClick={() => props.setOpen(false)}>
+          X
         </div>
-    )
-}
+        <div className="title">Add new user</div>
+        <form action="" className="newUserInfo" onSubmit={handleSubmit}>
+          <div className="allField">
+            {props.column
+              .filter((filter) => {
+                return (
+                  filter.headerName != "ID" &&
+                  filter.headerName != "Action" &&
+                  filter.headerName != "Age"
+                );
+              })
+              .map((column) => {
+                return (
+                  <div className="userBox" key={column.field}>
+                    <div className="label"> {column.headerName}</div>
+                    <input
+                      name={column.field}
+                      type={column.type}
+                      placeholder={column.field}
+                      className="userInput"
+                    />
+                  </div>
+                );
+              })}
+            <label className="uploadImg">
+              <div>Upload Avatar Image:</div>
+              <img src="img.svg" alt="" />
+              <input type="file" accept="image*" style={{ display: "none" }} />
+            </label>
+          </div>
+
+          <button> Send </button>
+        </form>
+      </div>
+    </div>
+  );
+};
--- admin-dashboard-react/src/components/navbar/Navbar.tsx
+++ admin-dashboard-react/src/components/navbar/Navbar.tsx
@@ -1,24 +1,75 @@
-import "./navbar.scss"
+import { useState } from "react";
+import "./navbar.scss";
+
+const notificationData = [
+  {
+    id: 1,
+    title: "New user registered",
+    message: "Thanh Hoang just created an account.",
+    time: "2 min ago",
+    isRead: false,
+  },
+  {
+    id: 2,
+    title: "New order received",
+    message: "Order #1024 is waiting for review.",
+    time: "15 min ago",
+    isRead: false,
+  },
+  {
+    id: 3,
+    title: "Server update",
+    message: "Dashboard data was updated successfully.",
+    time: "1 hour ago",
+    isRead: true,
+  },
+];
 
 export const Navbar = () => {
+  const [open, setOpen] = useState(false);
+
+  const unreadCount = notificationData.filter((item) => !item.isRead).length;
+
   return (
     <div className="navbar">
       <div className="logo">
         <img src="logo2.jpg" alt="logo" />
         <span>THANH HOANG</span>
       </div>
+
       <div className="icons">
-        <div className="notifications">
-            <img src="notifications.svg" alt="" />
-            <span>1</span>
+        <div className="notifications" onClick={() => setOpen(!open)}>
+          <img src="notifications.svg" alt="notifications" />
+          {!open && <span>{unreadCount}</span>}
+
+          {open && (
+            <div className="notificationDropdown">
+              <h3>Notifications</h3>
+
+              {notificationData.map((item) => (
+                <div
+                  className={
+                    item.isRead ? "notificationItem" : "notificationItem unread"
+                  }
+                  key={item.id}>
+                  <div>
+                    <strong>{item.title}</strong>
+                    <p>{item.message}</p>
+                    <small>{item.time}</small>
+                  </div>
+                </div>
+              ))}
+            </div>
+          )}
         </div>
-       
+
         <div className="user">
-          <img src="avatar.jpg" />
+          <img src="avatar.jpg" alt="avatar" />
           <span>Peter</span>
         </div>
-        <img src="setting.svg" alt="" />
-      </div>    
+
+        <img src="setting.svg" alt="settings" />
+      </div>
     </div>
-  )
-}
+  );
+};
